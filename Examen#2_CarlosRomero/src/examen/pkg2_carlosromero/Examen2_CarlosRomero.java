f77aed8a5160ec35e2427eeabbfd4a04823d817e
Will <Will@DESKTOP-E6E62EP>
2017-12-15 13:06:35 -0600

    Creacion

---
+++ Examen#2_CarlosRomero/src/examen/pkg2_carlosromero/Examen2_CarlosRomero.java
@@ -0,0 +1,16 @@
+package examen.pkg2_carlosromero;
+
+/**
+ *
+ * @author Will
+ */
+public class Examen2_CarlosRomero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
